15edc22e4bde793be506a1651420d7358ed94ae8
Mark Hamstra <hello@markhamstra.com>
2021-08-05 08:38:00 +0200

    Improve bootstrap a bit to behave better when there are conflicts

--- core/components/guzzle7/bootstrap.php
+++ core/components/guzzle7/bootstrap.php
@@ -2,12 +2,14 @@
 
 // MODX3 (as of alpha4) ships with Guzzle already, so we do absolutely nothing.
 // Technically this breaks the package on alpha versions before 3
-if (isset($modx) && $modx->services instanceof \Psr\Container\ContainerInterface) {
+if (isset($modx) && $modx->services instanceof \Psr\Container\ContainerInterface && $modx->services->has(\Psr\Http\Client\ClientInterface::class)) {
     return;
 }
 
 // Make sure Guzzle is not already loaded through another package, and don't load our version if that's the case
-if (class_exists(\GuzzleHttp\Client::class)) {
+// We check for both the Client and the ClientInterface primarily to load them into memory: otherwise conflicting
+// installations may use our loaded Client, but see a different (loaded later) ClientInterface and fail.
+if (class_exists(\GuzzleHttp\Client::class) && class_exists(\GuzzleHttp\ClientInterface::class)) {
     return;
 }
 
@@ -15,3 +17,5 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
     require_once __DIR__ . '/vendor/autoload.php';
 }
 
+// Load the classes; this time from our own location.
+$loaded = class_exists(\GuzzleHttp\Client::class) && class_exists(\GuzzleHttp\ClientInterface::class);
